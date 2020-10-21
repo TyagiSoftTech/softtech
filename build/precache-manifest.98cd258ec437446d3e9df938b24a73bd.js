5478109ca93962469f6de60faa7cfa7cb560f2a9
Dinesh Tyagi <dinesh777tyagi@gmail.com>
2020-10-21 18:02:18 +0530

    second commit tyagisofttech

---
+++ build/precache-manifest.98cd258ec437446d3e9df938b24a73bd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99da7eb257081c95fd541e52e5021f4",
+    "url": "/softtech/index.html"
+  },
+  {
+    "revision": "ee71294f7191f31b926e",
+    "url": "/softtech/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "feee946191c719978428",
+    "url": "/softtech/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "ee71294f7191f31b926e",
+    "url": "/softtech/static/js/2.5b3f3a08.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/softtech/static/js/2.5b3f3a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feee946191c719978428",
+    "url": "/softtech/static/js/main.45674f00.chunk.js"
+  },
+  {
+    "revision": "25788a664d24424bac16",
+    "url": "/softtech/static/js/runtime-main.da8b1623.js"
+  },
+  {
+    "revision": "e07b0975116d9c9b918bcb36f6223a5f",
+    "url": "/softtech/static/media/1.e07b0975.jpg"
+  },
+  {
+    "revision": "a74f4a4997e4ac8c15a63e97fa25192b",
+    "url": "/softtech/static/media/11.a74f4a49.jpg"
+  },
+  {
+    "revision": "687afc63c310381368561084ccfb8536",
+    "url": "/softtech/static/media/12.687afc63.jpg"
+  },
+  {
+    "revision": "f885bb1d69ef9364e5c5a78fe0bc0861",
+    "url": "/softtech/static/media/3.f885bb1d.jpg"
+  },
+  {
+    "revision": "ca8a1372e1523bf50a1efd3248d942b0",
+    "url": "/softtech/static/media/4.ca8a1372.webp"
+  },
+  {
+    "revision": "ebf9cb4d253a57db6eadbe4df9398afa",
+    "url": "/softtech/static/media/about.ebf9cb4d.jpg"
+  },
+  {
+    "revision": "0cf063a5dfcd4d66211e431ac5bdfb23",
+    "url": "/softtech/static/media/app.0cf063a5.jpg"
+  },
+  {
+    "revision": "154aa89d7dbf48c60b18b632036d675e",
+    "url": "/softtech/static/media/cloud.154aa89d.jpg"
+  },
+  {
+    "revision": "552b441962514456572015e97bc666c1",
+    "url": "/softtech/static/media/mobile.552b4419.jpg"
+  }
+]);
